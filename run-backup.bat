65f2fbe2a17aa320e2dc438ee136b6c60cf1c426
Rhys Connor <rhyskconnor99@gmail.com>
2022-03-16 01:00:38 +0000

    Missed }

--- run-backup.bat
+++ run-backup.bat
@@ -2,7 +2,7 @@ echo off
 echo Close MS Word to backup report!
 pause
 echo Copying file from OneDrive to local
-"C:/Users/{your user}/AppData/Local/Programs/Python/Python37-32/python.exe" "{path to your copy of BACKUP.py"
+"C:/Users/{your user}/AppData/Local/Programs/Python/Python37-32/python.exe" "{path to your copy of BACKUP.py}"
 echo Pushing changes to git repo 
 git add -A
 git commit -m "Backup on %date% %time%"
